242dc37530b895b39615b28756e7d5c3af28b50f
Cliff Foster <korewananda@gmail.com>
2019-12-15 13:45:44 -0800

    Simple profiler

--- doorstop/core/reference_finder.py
+++ doorstop/core/reference_finder.py
@@ -6,10 +6,40 @@ import os
 import re
 import linecache
 
+import time
+
 from doorstop import common, settings
 from doorstop.common import DoorstopError
 
 log = common.logger(__name__)
+
+class SimpleProfiler:
+
+    def __init__(self):
+        self.total_time = 0
+        self.iterations = 0
+        self.current_time = 0
+        self.min_time = 1000
+        self.max_time = -1000
+
+    def start_timer(self):
+        self.current_time = time.clock()
+
+    def end_time(self):
+        exec_time = time.clock() - self.current_time
+        self.min_time = min(exec_time, self.min_time)
+        self.max_time = max(exec_time, self.max_time)
+        self.total_time += exec_time
+        self.iterations += 1
+
+    def display_stats(self, title):
+        print(title)
+        print("\tIterations:", self.iterations)
+        print("\tTotal Time:", self.total_time)
+        print("\tMax:", self.max_time)
+        print("\tMin:", self.min_time)
+        print("\tAvg:", self.total_time/self.iterations)
+
 
 
 class ReferenceFinder:
@@ -28,7 +58,7 @@ class ReferenceFinder:
             filename) or None (when no reference set)
 
         """
-
+        profiler = SimpleProfiler()
         # Search for the external reference
         log.debug("searching for ref '{}'...".format(ref))
         pattern = r"(\b|\W){}(\b|\W)".format(re.escape(ref))
@@ -45,6 +75,7 @@ class ReferenceFinder:
             if os.path.splitext(filename)[-1] in settings.SKIP_EXTS:
                 continue
             # Search for the reference in the file
+            profiler.start_timer()
             lineno = 1
             while True:
                 try:
@@ -57,9 +88,12 @@ class ReferenceFinder:
                     break
                 if regex.search(line):
                     log.debug("found ref: {}".format(relpath))
+                    profiler.end_time()
+                    profiler.display_stats(f"Stats for: {item_path}")
                     return relpath, lineno
                 lineno += 1
-
+            profiler.end_time()
+        profiler.display_stats(f"Stats for: {item_path}")
         msg = "external reference not found: {}".format(ref)
         raise DoorstopError(msg)
 
